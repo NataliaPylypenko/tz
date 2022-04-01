81f766b03b1bb362966275e04843939833209012
NataliaPylypenko <natalia28ivanivna@gmail.com>
2022-04-01 18:48:49 +0200

    added index.html

---
+++ tz.js
@@ -0,0 +1,29 @@
+function combine(k, ls, start, itemArray, sumArr) {
+    if (k === 0) {
+        sumArr.push(itemArray.reduce( (a, b) => a + b));
+        return;
+    }
+
+    for (let i = start; i <= ls.length - k; i++) {
+        itemArray[itemArray.length - k] = ls[i];
+        combine(k - 1, ls, i + 1, itemArray, sumArr);
+    }
+}
+
+function chooseOptimalDistance(t, k, ls) {
+    let sumArr = [];
+    let itemArray = [];
+    itemArray.length = k;
+
+    combine(k, ls, 0, itemArray, sumArr);
+
+    let res = sumArr.sort((a, b) => b - a).filter(item => item <= t).shift();
+
+    if (res) {
+        return res;
+    } else {
+        return null;
+    }
+}
+
+console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]));
