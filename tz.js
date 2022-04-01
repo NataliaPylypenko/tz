588b13238c8a9598f6a233b4eb249b4ce2be3772
NataliaPylypenko <natalia28ivanivna@gmail.com>
2022-04-01 20:00:30 +0200

    chooseOptimalDistance

--- tz.js
+++ tz.js
@@ -11,19 +11,15 @@ function combine(k, ls, start, itemArray, sumArr) {
 }
 
 function chooseOptimalDistance(t, k, ls) {
-    let sumArr = [];
-    let itemArray = [];
+    const sumArr = [];
+    const itemArray = [];
     itemArray.length = k;
 
     combine(k, ls, 0, itemArray, sumArr);
 
     let res = sumArr.sort((a, b) => b - a).filter(item => item <= t).shift();
 
-    if (res) {
-        return res;
-    } else {
-        return null;
-    }
+    return res ? res : null
 }
 
 console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]));
